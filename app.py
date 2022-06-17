beccc08ae6e31b9df49212a51f59296309e14fa2
Adlin Vladislav <adlin@lanit.ru>
2022-06-17 06:28:23 +0300

    blob store and file data

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask, render_template
+import urllib.request
+from io import StringIO
+from flask import Flask, render_template, abort
 import pandas as pd
+from azure.storage.blob import ContainerClient
 
 app = Flask(__name__)
 
-sample_data = pd.read_csv('us-500.csv')
+blob_url = 'https://vdadlin.blob.core.windows.net/csvtable/us-500.csv'
+conn_str="DefaultEndpointsProtocol=https;AccountName=vdadlin;AccountKey=MpdP8qfkZhz9MnTPWAFweHg4f0FP86j3MTLcyQ28l99c+OGHkIqii9pT4IQybq3UHNmdr5lJZ2Tw+AStYaikOQ==;EndpointSuffix=core.windows.net"
+container_name="csvtable"
 
 @app.route("/")
 def sample_table():
+    try:
+
+        container_client=ContainerClient.from_connection_string(conn_str,container_name)
+        blobs = container_client.list_blobs()
+        blob_modified = 'unknown'
+
+        for blob in blobs:
+            if blob.name == 'us-500.csv':
+                blob_modified = blob.last_modified.strftime('%d/%m/%y %H:%M:%S')
+                break
+
+        response = urllib.request.urlopen(blob_url).read()
+
+    except Exception as e:
+        abort(404)
+        
+    contents=str(response,'utf-8')
+    sample_data = pd.read_csv(StringIO(contents))
     columns = sample_data.columns
     values = sample_data.values
-    return render_template('table.html', columns=columns, values=values, title="SAMPLE DATA STAGE")
+    return render_template('table.html', columns=columns, values=values, title="SAMPLE DATA STAGE (last modified {})".format(blob_modified))
 
 if __name__ == '__main__':
     app.run()
--- message-teams.ps1
+++ message-teams.ps1
@@ -8,12 +8,13 @@ function Send-Message {
         [string]$AgentJobStatus,
         [string]$TriggeredBy,
         [string]$SummaryUri
+        [string]$targetEnv
     )
     $message = [PSCustomObject]@{
         '@type' = "MessageCard"
         '@context' = "https://schema.org/extensions"
         "themeColor" = if ($AgentJobStatus -eq "Succeeded") {"008000"} else {"ff0000"}
-        "title" = "$DefinitionName ${Number}: **$AgentJobStatus**"
+        "title" = "$DefinitionName ${Number} $targetEnv: **$AgentJobStatus**"
         "text" = "Triggered by $TriggeredBy"
         "potentialAction" = @(@{
             '@type' = "OpenUri"
@@ -25,16 +26,10 @@ function Send-Message {
         })
     }
 
-    # Invoke-RestMethod -Method post -ContentType 'Application/Json' -Body (ConvertTo-Json -Compress -Depth 5 -InputObject $message) -Uri $teamsWebhookUrl
+    Invoke-RestMethod -Method post -ContentType 'Application/Json' -Body (ConvertTo-Json -Compress -Depth 5 -InputObject $message) -Uri $teamsWebhookUrl
     Write-Host "Json output:"
     Write-Host (ConvertTo-Json -Compress -Depth 5 -InputObject $message)
 }
-
-# Azure DevOps Variables
-# Build variables
-# https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=vsts
-# Release variables
-# https://docs.microsoft.com/en-us/azure/devops/pipelines/release/variables?view=vsts&tabs=batch#view-vars
 
 $AgentJobStatus = ${env:agent.jobstatus}
 if ($env:RELEASE_DEFINITIONNAME) {
@@ -55,18 +50,19 @@ else {
 $targetEnv = if ($env.isMaster -eq 'True') {'stage'} else {'prod'}
 
 # for testing
-Write-Host "Output of variables:"
-Write-Host "AgentJobstatus" $AgentJobstatus
-Write-Host "Number" $Number
-Write-Host "DefinitionName" $DefinitionName
-Write-Host "TriggeredBy" $TriggeredBy
-Write-Host "SummaryUri" $SummaryUri
-Write-Host "Target enviroment" $targetEnv
+# Write-Host "Output of variables:"
+# Write-Host "AgentJobstatus" $AgentJobstatus
+# Write-Host "Number" $Number
+# Write-Host "DefinitionName" $DefinitionName
+# Write-Host "TriggeredBy" $TriggeredBy
+# Write-Host "SummaryUri" $SummaryUri
+# Write-Host "Target enviroment" $targetEnv
 
 # Run
-# Send-Message `
-# -Number $Number `
-# -DefinitionName $DefinitionName `
-# -AgentJobStatus $AgentJobStatus `
-# -TriggeredBy $TriggeredBy `
-# -SummaryUri $SummaryUri
+Send-Message `
+-Number $Number `
+-DefinitionName $DefinitionName `
+-AgentJobStatus $AgentJobStatus `
+-TriggeredBy $TriggeredBy `
+-SummaryUri $SummaryUri
+-targetEnv $targetEnv
